ad8fe5d29b4cc0da15f3980021d4b45823e489b8
MGLFBASSTOURNAMENT <sho8888.sidewinder@gmail.com>
2026-07-08 19:34:01 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-const CACHE_NAME = 'mglf-v1';
-const ASSETS = [
+const CACHE_NAME = 'mglf-cache-v3';
+const urlsToCache = [
   'mglf_batch_app.html',
-  'manifest.json'
+  'manifest.json',
+  'appstore.png',
+  'playstore.png'
 ];
 
-// インストール時にHTMLをスマホに強制保存
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// オフライン時は、ネットではなくスマホ内のキャッシュから画面を出す
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
